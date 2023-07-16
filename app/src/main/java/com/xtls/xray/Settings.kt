869c385efcf090a5cc6493a360002bd1b5c7b80a
SaeedPooyanfar <saeedp47@gmail.com>
2023-07-16 19:34:54 +0330

    default DNS: CF

--- app/src/main/java/com/xtls/xray/Settings.kt
+++ app/src/main/java/com/xtls/xray/Settings.kt
@@ -3,7 +3,7 @@ package com.xtls.xray
 object Settings {
     var socksAddress: String = "127.0.0.1"
     var socksPort: String = "10808"
-    var primaryDns: String = "8.8.8.8"
-    var secondaryDns: String = "8.8.4.4"
+    var primaryDns: String = "1.1.1.1"
+    var secondaryDns: String = "1.0.0.1"
     var useXray: Boolean = false
 }
