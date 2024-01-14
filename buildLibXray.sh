9b42c97eacf63f389e7b9d91fbf1cc7af6c2e315
SaeedDev94 <saeedp47@gmail.com>
2024-01-14 22:52:34 +0330

    buildLibXray script

---
+++ buildLibXray.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DEST="../app/libs"
+
+prepare_go() {
+  go install golang.org/x/mobile/cmd/gomobile@latest
+  go get golang.org/x/mobile/bind
+  go mod download
+}
+
+build_android() {
+  rm -f "$DEST/*.jar"
+  rm -f "$DEST/*.aar"
+  gomobile init
+  gomobile bind -o "$DEST/libXray.aar" -target android -androidapi 29
+}
+
+cd libXray
+prepare_go
+build_android
