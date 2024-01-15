ea10d2c206ce32a13034174fab8431acca9a2eed
SaeedDev94 <saeedp47@gmail.com>
2024-01-15 19:56:18 +0330

    modify DPKGNAME

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -16,6 +16,6 @@
 APP_OPTIM := release
 APP_PLATFORM := android-29
 APP_ABI := armeabi-v7a arm64-v8a
-APP_CFLAGS := -O3 -DPKGNAME=com/xtls/xray
+APP_CFLAGS := -O3 -DPKGNAME=io/github/saeeddev94/xray
 APP_CPPFLAGS := -O3 -std=c++11
 NDK_TOOLCHAIN_VERSION := clang
