bc794156d0352e35390e1c5683a1a2c97b8a1adb
Saeed <saeedp47@gmail.com>
2024-01-30 15:41:25 +0330

    add android sdk build-tools to PATH

--- build-xray.sh
+++ build-xray.sh
@@ -22,13 +22,6 @@ apt-get install -y git openjdk-$JAVA_VERSION-jdk-headless sdkmanager wget unzip 
 sdkmanager "platform-tools" "platforms;$ANDROID_PLATFORM_VERSION" "build-tools;$ANDROID_SDK_VERSION"
 sdkmanager --install "ndk;$ANDROID_NDK_VERSION" --channel=3
 
-# Set vars
-export ANDROID_HOME="/opt/android-sdk"
-export ANDROID_NDK_HOME="$ANDROID_HOME/ndk/$ANDROID_NDK_VERSION"
-export JAVA_HOME="/usr/lib/jvm/java-$JAVA_VERSION-openjdk-amd64"
-export PATH="$JAVA_HOME/bin:$PATH"
-export PATH="$PATH:$ANDROID_HOME/platform-tools"
-
 # Create directories
 mkdir -p /home/vagrant/gradle
 mkdir -p /home/vagrant/build/srclib
@@ -40,7 +33,6 @@ wget "https://services.gradle.org/distributions/$GRADLE_ARCHIVE"
 unzip "$GRADLE_ARCHIVE"
 rm "$GRADLE_ARCHIVE"
 mv * "$GRADLE_VERSION"
-export PATH="/home/vagrant/gradle/$GRADLE_VERSION/bin:$PATH"
 popd
 
 # Build go
@@ -49,9 +41,20 @@ pushd /home/vagrant/build/srclib/go
 git checkout "$GO_VERSION"
 cd src
 ./make.bash
-export GOPATH="/home/vagrant/build/srclib/go"
-export PATH="$GOPATH/bin:$PATH"
 popd
+
+# Set vars
+export JAVA_HOME="/usr/lib/jvm/java-$JAVA_VERSION-openjdk-amd64"
+export ANDROID_HOME="/opt/android-sdk"
+export ANDROID_NDK_HOME="$ANDROID_HOME/ndk/$ANDROID_NDK_VERSION"
+export GOPATH="/home/vagrant/build/srclib/go"
+
+# Set path
+export PATH="$JAVA_HOME/bin:$PATH"
+export PATH="/home/vagrant/gradle/$GRADLE_VERSION/bin:$PATH"
+export PATH="$ANDROID_HOME/platform-tools:$PATH"
+export PATH="$ANDROID_HOME/build-tools/$ANDROID_SDK_VERSION:$PATH"
+export PATH="$GOPATH/bin:$PATH"
 
 # Clone repo
 git clone https://github.com/SaeedDev94/Xray.git /home/vagrant/build/io.github.saeeddev94.xray
@@ -76,15 +79,14 @@ popd
 gradle -PabiId=$ABI_ID -PabiTarget=$ABI_TARGET assembleRelease
 
 # Sign app
-ANDROID_SDK_TOOLS="$ANDROID_HOME/build-tools/$ANDROID_SDK_VERSION"
 VERSION_CODE=$(cat versionCode.txt)
 ((VERSION_CODE += ABI_ID))
 BUILD_NAME="Xray-$RELEASE_TAG-$VERSION_CODE.apk"
 cd build/outputs/apk/release
 echo "$KS_FILE" > /xray_base64.txt
 base64 -d /xray_base64.txt > /xray.jks
-$ANDROID_SDK_TOOLS/zipalign -p -f -v 4 "app-$ABI_TARGET-release-unsigned.apk" "$BUILD_NAME"
-$ANDROID_SDK_TOOLS/apksigner sign --ks /xray.jks --ks-pass "pass:$KS_PASSWORD" --ks-key-alias "$KEY_ALIAS" --key-pass "pass:$KEY_PASSWORD" "$BUILD_NAME"
+zipalign -p -f -v 4 "app-$ABI_TARGET-release-unsigned.apk" "$BUILD_NAME"
+apksigner sign --ks /xray.jks --ks-pass "pass:$KS_PASSWORD" --ks-key-alias "$KEY_ALIAS" --key-pass "pass:$KEY_PASSWORD" "$BUILD_NAME"
 
 # Move app to dist dir
 mv "$BUILD_NAME" "$DIST_DIR"
