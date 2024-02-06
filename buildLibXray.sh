90dfda0d0819a005e068127b41ba12a4d7d2e3ce
SaeedDev94 <saeedp47@gmail.com>
2024-02-06 11:00:26 +0330

    buildLibXray.sh: build only for 1 arch

--- buildLibXray.sh
+++ buildLibXray.sh
@@ -1,19 +1,50 @@
 #!/bin/bash
 
+TARGET="$1"
+ARCHS=(arm arm64 386 amd64)
 DEST="../app/libs"
 
+is_in_array() {
+  local value="$1"
+  local array=("${@:2}")
+
+  for item in "${array[@]}"; do
+    if [[ "$item" == "$value" ]]; then
+      return 0
+    fi
+  done
+
+  return 1
+}
+
+check_target() {
+  if ! is_in_array "$TARGET" "${ARCHS[@]}"; then
+    echo "Not supported"
+    exit 1
+  fi
+}
+
 prepare_go() {
+  echo "Install dependencies"
+  cd libXray
   go install golang.org/x/mobile/cmd/gomobile@v0.0.0-20240112133503-c713f31d574b
   go mod download
 }
 
 build_android() {
-  rm -f "$DEST/*.jar"
-  rm -f "$DEST/*.aar"
+  echo "Building libXray for $TARGET"
+  rm -f "$DEST/libXray*"
   gomobile init
-  gomobile bind -o "$DEST/libXray.aar" -target android -androidapi 29 -ldflags="-buildid=" -trimpath
+  gomobile bind -o "$DEST/libXray.aar" -androidapi 29 -target android/$TARGET -ldflags="-buildid=" -trimpath
 }
 
-cd libXray
+refresh_dependencies() {
+  echo "Gradle: refresh dependencies"
+  cd ..
+  ./gradlew --refresh-dependencies clean
+}
+
+check_target
 prepare_go
 build_android
+refresh_dependencies
