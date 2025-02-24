947b8d5c8191feaf4711e47b3f9ea9236bad29f0
SaeedDev94 <saeedp47@gmail.com>
2025-02-24 06:15:57 -0700

    Xray-core @ v25.2.21

--- buildXrayCore.sh
+++ buildXrayCore.sh
@@ -34,7 +34,7 @@ prepare_go() {
   # go mod tidy
   # go get golang.org/x/mobile
   # go get google.golang.org/genproto
-  go install golang.org/x/mobile/cmd/gomobile@v0.0.0-20241213221354-a87c1cf6cf46
+  go install golang.org/x/mobile/cmd/gomobile@v0.0.0-20250218173827-cd096645fcd3
   go mod download
 }
 
