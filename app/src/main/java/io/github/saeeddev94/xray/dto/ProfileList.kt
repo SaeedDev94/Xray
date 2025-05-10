dd6ee1510179c75e878529daf37d9e080ff8a98b
SaeedDev94 <saeedp47@gmail.com>
2025-05-10 08:51:55 -0700

    ProfileList: rm unused fromProfile fn

--- app/src/main/java/io/github/saeeddev94/xray/dto/ProfileList.kt
+++ app/src/main/java/io/github/saeeddev94/xray/dto/ProfileList.kt
@@ -1,13 +1,7 @@
 package io.github.saeeddev94.xray.dto
 
-import io.github.saeeddev94.xray.database.Profile
-
 data class ProfileList(
     var id: Long,
     var index: Int,
     var name: String,
-) {
-    companion object {
-        fun fromProfile(value: Profile) = ProfileList(value.id, value.index, value.name)
-    }
-}
+)
