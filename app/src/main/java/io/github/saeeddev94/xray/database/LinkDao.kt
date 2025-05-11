37effade89f56a9115cd3d4f5b8d2d67dcd61953
SaeedDev94 <saeedp47@gmail.com>
2025-05-11 02:44:15 -0700

    links: order by id asc

--- app/src/main/java/io/github/saeeddev94/xray/database/LinkDao.kt
+++ app/src/main/java/io/github/saeeddev94/xray/database/LinkDao.kt
@@ -9,13 +9,13 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface LinkDao {
-    @Query("SELECT * FROM links ORDER BY id DESC")
+    @Query("SELECT * FROM links ORDER BY id ASC")
     fun all(): Flow<List<Link>>
 
-    @Query("SELECT * FROM links WHERE is_active = 1 ORDER BY id DESC")
+    @Query("SELECT * FROM links WHERE is_active = 1 ORDER BY id ASC")
     fun tabs(): Flow<List<Link>>
 
-    @Query("SELECT * FROM links WHERE is_active = 1 ORDER BY id DESC")
+    @Query("SELECT * FROM links WHERE is_active = 1 ORDER BY id ASC")
     suspend fun activeLinks(): List<Link>
 
     @Insert
