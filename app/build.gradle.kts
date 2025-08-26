75dd63a8270063bb36514782a4cc0d763086036e
SaeedDev94 <saeedp47@gmail.com>
2025-08-26 09:10:42 -0700

    v11.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = calcVersionCode()
-        versionName = "11.3.1"
+        versionName = "11.3.2"
 
         vectorDrawables {
             useSupportLibrary = true
