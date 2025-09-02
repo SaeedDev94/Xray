e72c6ac178dab8fa3b238cdc86b9b33a9fb1b72a
SaeedDev94 <saeedp47@gmail.com>
2025-09-02 12:19:46 -0700

    v11.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = calcVersionCode()
-        versionName = "11.4.1"
+        versionName = "11.5.0"
     }
 
     buildFeatures {
