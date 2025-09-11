3a6cc46c0d235aa26a954ae3ca105790b8794897
SaeedDev94 <saeedp47@gmail.com>
2025-09-11 11:15:15 -0700

    Fix profiles filter issue on app open

--- app/src/main/java/io/github/saeeddev94/xray/viewmodel/LinkViewModel.kt
+++ app/src/main/java/io/github/saeeddev94/xray/viewmodel/LinkViewModel.kt
@@ -5,26 +5,14 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import io.github.saeeddev94.xray.Xray
 import io.github.saeeddev94.xray.database.Link
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class LinkViewModel(application: Application) : AndroidViewModel(application) {
 
     private val linkRepository by lazy { getApplication<Xray>().linkRepository }
 
-    val tabs = linkRepository.tabs.flowOn(Dispatchers.IO).stateIn(
-        viewModelScope,
-        SharingStarted.Eagerly,
-        listOf(),
-    )
-    val links = linkRepository.all.flowOn(Dispatchers.IO).stateIn(
-        viewModelScope,
-        SharingStarted.Eagerly,
-        listOf(),
-    )
+    val tabs = linkRepository.tabs
+    val links = linkRepository.all
 
     suspend fun activeLinks(): List<Link> {
         return linkRepository.activeLinks()
