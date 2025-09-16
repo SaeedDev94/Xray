77b5906dc7c50f6f05789aac303f1008e31fc5fa
SaeedDev94 <saeedp47@gmail.com>
2025-09-16 00:47:28 -0700

    dynamic gradle version

--- build-xray.sh
+++ build-xray.sh
@@ -7,12 +7,18 @@ echo "deb https://deb.debian.org/debian bookworm-backports main" > /etc/apt/sour
 apt-get update || apt-get update
 apt-get dist-upgrade -y
 
+# Gradle version
+GRADLE_URL=$(grep distributionUrl gradle/wrapper/gradle-wrapper.properties | \
+  cut -d '=' -f 2 | \
+  sed 's#\\##g')
+GRADLE_ARCHIVE=$(basename $GRADLE_URL)
+GRADLE_VERSION=$(echo "$GRADLE_ARCHIVE" | sed -E 's/gradle-([0-9.]+)-bin\.zip/\1/')
+
 # Tools version
 ANDROID_PLATFORM_VERSION="android-35"
 ANDROID_SDK_VERSION="35.0.0"
 ANDROID_NDK_VERSION="28.2.13676358"
 JAVA_VERSION="17"
-GRADLE_VERSION="8.14.3"
 
 # Install Tools
 apt-get install -t bookworm-backports -y golang-go
@@ -40,8 +46,7 @@ export PATH="$ANDROID_HOME/build-tools/$ANDROID_SDK_VERSION:$PATH"
 # Download gradle
 mkdir -p $GRADLE_DIR
 pushd $GRADLE_DIR
-GRADLE_ARCHIVE="gradle-$GRADLE_VERSION-bin.zip"
-wget "https://services.gradle.org/distributions/$GRADLE_ARCHIVE"
+wget "$GRADLE_URL"
 unzip "$GRADLE_ARCHIVE"
 rm "$GRADLE_ARCHIVE"
 mv * "$GRADLE_VERSION"
@@ -52,6 +57,10 @@ git clone https://github.com/SaeedDev94/Xray.git $REPO_DIR
 cd $REPO_DIR
 git checkout "$RELEASE_TAG"
 git submodule update --init --recursive
+
+# Clean task
+rm gradle/wrapper/gradle-wrapper.jar
+gradle clean
 
 # Build dependencies
 ./buildGo.sh $NATIVE_ARCH
